9442f50290bcb9f60fdd7378606b1291e51e271b
Shubhangi Pandey <103631803+Shubhangi3108@users.noreply.github.com>
2022-09-29 18:06:04 +0530

    Update W4pro8.cpp

--- W4pro8.cpp
+++ W4pro8.cpp
@@ -2,28 +2,28 @@
 using namespace std;
  
 //Class declaration.
-class Demo
+class Demonstrate
 {
     private:
         int X;
         int Y;
     public:
-            Demo (int a, int b)
+            Demonstrate (int a, int b)
         void    Input();
         void    Display();
      
 };
-Demo:: Demo(int a, int b)
+ Demonstrate(int a, int b)
 {
     X = a;
     Y = b;
 }
-void Demo:: Input()
+void Demonstrate:: Input()
 {
     cout << "Enter Value of X: "; cin >> X;
     cout << "Enter Value of Y: "; cin >> Y;
 }
-void Demo:: Display()
+void Demonstrate:: Display()
 {
     cout << endl << "X: " << X;
     cout << endl << "Y: " << Y << endl;
@@ -31,14 +31,14 @@ void Demo:: Display()
  
 int main()
 {
-    Demo d(10,20) ;
+    Demonstrate d(10,20) ;
     cout << endl <<"Method 1: " << endl;  
     cout << "Value after initialization: " ;
     d.Display();    
     d.Input();
     cout << "Value after User Input : ";
     d.Display();
-    Demo d1 = Demo(10,20);
+    Demonstrate d1 = Demonstrate(10,20);
     cout << endl << "Method 2: " << endl;
     cout << "Value after initialization: ";
     d1.Display();   
