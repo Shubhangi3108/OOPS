3c8acc087fd229e484a4167341afd291f38bd207
Shubhangi Pandey <103631803+Shubhangi3108@users.noreply.github.com>
2022-11-07 09:16:24 +0530

    Update W9pro6.cpp

--- W9pro6.cpp
+++ W9pro6.cpp
@@ -4,19 +4,21 @@ using namespace std;
 template<class t>
  class sum
  {
-t Sum(t a,t b) 
+  t a,b;
+  public:
+  sum()
+  {
+   cin>>a>>b;
+  }
+  t Sum() 
 {
     cout<<"\nSum="<<a+b<<endl;
 }
  };
 int main()
 {
-   sum<int> intSum(a,b);
-    sum<float> floatSum(x,y);
-    cout<<"\nEnter two integer data: ";
-    cin>>a>>b;
-    cout<<"\nEnter two float data: ";
-    cin>>x>>y;
-    sum(a,b); 
-    sum(x,y);      
+   sum<int>obj_add;
+ obj_add.Sum();
+    sum<float>obj_add2;
+ obj_add2.Sum();    
 }
